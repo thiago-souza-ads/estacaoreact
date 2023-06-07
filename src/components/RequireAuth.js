2012bb8ba510f22be0ed3356f6c9801d199c9a37
EstacaoEnadeDouglas <estacaoenade@gmail.com>
2023-06-06 21:23:19 -0300

    Setando e recuperando usuario

--- src/components/RequireAuth.js
+++ src/components/RequireAuth.js
@@ -1,17 +1,29 @@
 import { useLocation, Navigate, Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
+import SideBar from "./sideBar/SideBar";
 
 const RequireAuth = ({ allowedRoles }) => {
     const { auth } = useAuth();
     const location = useLocation();
+    const { usuario } = auth;
+    const userData = usuario;
 
     return (
-        allowedRoles.includes(auth?.roles)
-            ? <Outlet />
-            : auth?.user
-                ? <Navigate to="/unauthorized" state={{ from: location }} replace />
-                : <Navigate to="/login" state={{ from: location }} replace />
+        <>
+            {allowedRoles.includes(auth?.roles) ? (
+                <>
+                    <div>
+                        <SideBar user={userData} />
+                        <Outlet />
+                    </div>
+                </>
+            ) : auth?.user ? (
+                <Navigate to="/unauthorized" state={{ from: location }} replace />
+            ) : (
+                <Navigate to="/login" state={{ from: location }} replace />
+            )}
+        </>
     );
-}
+};
 
 export default RequireAuth;
---
+++ src/components/sideBar/SideBar.js
@@ -0,0 +1,41 @@
+import React, { useContext, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import styles from './SideBar.module.css';
+import AuthContext from '../../context/AuthProvider';
+import { FiPower } from 'react-icons/fi';
+
+const SideBar = ({ user }) => {
+    const { setAuth, auth } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
+    const logout = async () => {
+        setAuth({});
+        navigate('/login');
+    };
+
+    const toggleSidebar = () => {
+        setSidebarOpen(!sidebarOpen);
+    };
+
+    return (
+        <div className={`${styles['user-sidebar']} ${sidebarOpen ? styles.open : ''}`}>
+            <div className={styles['profile-circle']}>
+                <img src="/path/to/user/profile/image" alt="User Profile" />
+            </div>
+            <h2>{user.nome}</h2>
+            <h3>{user.roles[0].nome}</h3>
+            <Link to="/linkpage">Editar Perfil</Link>
+            <div className="flexGrow">
+                <button className={styles['logout-button']} onClick={logout}>
+                    <FiPower size={16} /> Sair
+                </button>
+            </div>
+            <div className={styles['toggle-button']} onClick={toggleSidebar}>
+                <span>{sidebarOpen ? '<' : '>'}</span>
+            </div>
+        </div>
+    );
+};
+
+export default SideBar;
