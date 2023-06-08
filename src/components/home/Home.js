5f7f7e466f86f493e3564d7f3cfcb9e857bc7915
EstacaoEnadeDouglas <estacaoenade@gmail.com>
2023-06-07 21:46:03 -0300

    Home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,5 @@
-import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
+import {useNavigate} from "react-router-dom";
+import {useContext} from "react";
 import AuthContext from "../../context/AuthProvider";
 import style from './Home.module.css';
 import {HomeAdministrador} from "./homeImpl/HomeAdministrador";
@@ -9,8 +9,8 @@ import {HomeAluno} from "./homeImpl/HomeAluno";
 
 
 const Home = () => {
-    const { setAuth, auth } = useContext(AuthContext);
-    const { usuario } = auth;
+    const {setAuth, auth} = useContext(AuthContext);
+    const {usuario} = auth;
     const userData = usuario;
     const navigate = useNavigate();
     const role = userData.roles[0].name;
@@ -23,20 +23,19 @@ const Home = () => {
     const getComponent = () => {
         switch (role) {
             case "Administrador":
-                return <HomeAdministrador />;
+                return <HomeAdministrador/>;
             case "Coordenador":
-                return <HomeCoordenador />;
+                return <HomeCoordenador/>;
             case "Professor":
-                return <HomeProfessor />;
+                return <HomeProfessor/>;
             default:
-                return <HomeAluno />;
+                return <HomeAluno/>;
         }
     }
 
     return (
         <div className={style.fullScreen}>
             {getComponent()}
-            <button onClick={logout}>Sign Out</button>
         </div>
     );
 }
--- src/components/home/homeImpl/HomeAluno.js
+++ src/components/home/homeImpl/HomeAluno.js
@@ -1,4 +1,4 @@
-import {Navbar, Container, Row, Col, Nav} from "react-bootstrap";
+import {Card, Col, Container, Nav, Navbar, Row} from "react-bootstrap";
 import styles from "./HomeAluno.module.css";
 import {Link} from "react-router-dom";
 
@@ -6,45 +6,63 @@ export const HomeAluno = () => {
     return (
         <>
             <Navbar className={styles.navbar}>
-                    <Nav className={`mr-auto ${styles.navbarNav}`}>
-                        <Nav.Link as={Link} to="/usuarios">
-                            Usuários
-                        </Nav.Link>
-                        <Nav.Link as={Link} to="/coordenadores">
-                            Coordenadores
-                        </Nav.Link>
-                        <Nav.Link as={Link} to="/professores">
-                            Professores
-                        </Nav.Link>
-                        <Nav.Link as={Link} to="/alunos">
-                            Alunos
-                        </Nav.Link>
-                        <Nav.Link as={Link} to="/enade">
-                            Enade
-                        </Nav.Link>
-                    </Nav>
+                <Nav className={`mr-auto ${styles.navbarNav}`}>
+                    <Nav.Link as={Link} to="/usuarios">
+                        Usuários
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/coordenadores">
+                        Coordenadores
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/professores">
+                        Professores
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/alunos">
+                        Alunos
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/enade">
+                        Enade
+                    </Nav.Link>
+                </Nav>
             </Navbar>
             <Container fluid>
-                <Row className={styles.container}>
-                    <Col md={6} className={styles.column}>
-                        <h1>CAAAAAAAAAAAAAAAAAAn 1</h1>
-                        <p>ContenAAAAAAAAAAAAAAAAAAAAAAAAAx goes here.</p>
-                    </Col>
-                    <Col md={6} className={styles.column}>
-                        <h1>Column 2</h1>
-                        <p>CoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAhgfs  s here.</p>
-                    </Col>
-                </Row>
+                <Col md={12} className={styles.column12}>
+                    <Card style={{width: "40%", backgroundColor: "whitesmoke", height: "32rem", borderRadius: '10px'}}>
+                        <Card.Body>
+                            <Card.Title style={{color: "green"}}>GEEKSFORGEEKS</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                One Stop For all CS subjects
+                            </Card.Subtitle>
+                            <Card.Text>
+                                GeeksforGeeks provides a platform for all the students to study
+                                about all the subjects in CSE.
+                            </Card.Text>
+                            <Card.Link href="#"> For Students</Card.Link>
+                        </Card.Body>
+                    </Card>
+                    <Card style={{width: "40%", backgroundColor: "whitesmoke", height: "32rem", borderRadius: '10px'}}>
+                        <Card.Body>
+                            <Card.Title style={{color: "green"}}>GEEKSFORGEEKS</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                One Stop For all CS subjects
+                            </Card.Subtitle>
+                            <Card.Text>
+                                GeeksforGeeks provides a platform for all the students to study
+                                about all the subjects in CSE.
+                            </Card.Text>
+                            <Card.Link href="#"> For Students</Card.Link>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <footer className={`footer mt-auto py-3 bg-light ${styles.footer}`}>
+                    <Container fluid>
+                        <Row>
+                            <Col md={12}>
+                                <span className="text-muted">Aplicação criada pelo LTD-Estácio 2023.</span>
+                            </Col>
+                        </Row>
+                    </Container>
+                </footer>
             </Container>
-            <footer className={`footer mt-auto py-3 bg-light ${styles.footer}`}>
-                <Container fluid>
-                    <Row>
-                        <Col md={12}>
-                            <span className="text-muted">Footer content goes here.</span>
-                        </Col>
-                    </Row>
-                </Container>
-            </footer>
         </>
     );
 };
