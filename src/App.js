0c90f925870ed054e438c0c5a6a177bb850cfbbf
EstacaoEnadeDouglas <estacaoenade@gmail.com>
2023-06-15 23:39:22 -0300

    Avatar Edição

--- src/App.js
+++ src/App.js
@@ -9,85 +9,92 @@ import Unauthorized from './components/naoAutorizado/Unauthorized';
 import Lounge from './components/Lounge';
 import LinkPage from './components/LinkPage';
 import RequireAuth from './components/RequireAuth';
-import { Routes, Route } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import Usuarios from "./components/usuarios/Usuarios";
 import Dashboard from "./components/dashboard/Dashboard";
 import Alunos from "./components/alunos/Alunos";
 import Mapa from "./components/mapa/Mapa";
-import Questionario from "./components/questionarios/Questionarios";
 import Questionarios from "./components/questionarios/Questionarios";
+import AvatarEdit from "./components/avatarEdit/AvatarEdit";
 
 const ROLES = {
-  'Professor': 1001,
-  'Coordenador': 1002,
-  'Administrador': 1003,
-  'Aluno': 1000
+    'Professor': 1001,
+    'Coordenador': 1002,
+    'Administrador': 1003,
+    'Aluno': 1000
 }
 
 function App() {
 
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        {/* Rotas Publicas */}
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="linkpage" element={<LinkPage />} />
-        <Route path="unauthorized" element={<Unauthorized />} />
+    return (
+        <Routes>
+            <Route path="/" element={<Layout/>}>
+                {/* Rotas Publicas */}
+                <Route path="login" element={<Login/>}/>
+                <Route path="register" element={<Register/>}/>
+                <Route path="linkpage" element={<LinkPage/>}/>
+                <Route path="unauthorized" element={<Unauthorized/>}/>
 
-        {/* Rotas Protegidas */}
-
-
-        {/* Mista entre ususarios */}
-        <Route element={<RequireAuth allowedRoles={[ROLES.Aluno, ROLES.Administrador, ROLES.Coordenador, ROLES.Professor]} />}>
-          <Route path="/" element={<Home />} />
-        </Route>
-
-        <Route element={<RequireAuth allowedRoles={[ROLES.Administrador, ROLES.Coordenador, ROLES.Professor]} />}>
-          <Route path="editor" element={<Editor />} />
-        </Route>
-
-        <Route element={<RequireAuth allowedRoles={[ROLES.Administrador, ROLES.Coordenador]} />}>
-          <Route path="admin" element={<Admin />} />
-        </Route>
-
-        <Route element={<RequireAuth allowedRoles={[ROLES.Administrador, ROLES.Coordenador, ROLES.Professor]} />}>
-          <Route path="lounge" element={<Lounge />} />
-        </Route>
-
-        <Route element={<RequireAuth allowedRoles={[ROLES.Administrador, ROLES.Coordenador]} />}>
-          <Route path="/usuarios" element={<Usuarios />} />
-        </Route>
-
-        <Route element={<RequireAuth allowedRoles={[ROLES.Administrador, ROLES.Coordenador]} />}>
-          <Route path="/alunos" element={<Alunos />} />
-        </Route>
-
-        <Route element={<RequireAuth allowedRoles={[ROLES.Administrador, ROLES.Coordenador]} />}>
-          <Route path="/mapas" element={<Mapa />} />
-        </Route>
-
-        <Route element={<RequireAuth allowedRoles={[ROLES.Administrador, ROLES.Coordenador]} />}>
-          <Route path="/questionarios" element={<Questionarios />} />
-        </Route>
-
-        {/* Somente para Administradores */}
-        <Route element={<RequireAuth allowedRoles={[ROLES.Administrador]} />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-
-        {/* Somente para Coordenadores */}
-
-        {/* Somente para Professores */}
-
-        {/* Somente para Alunos */}
+                {/* Rotas Protegidas */}
 
 
-        {/* Liberada */}
-        <Route path="*" element={<Missing />} />
-      </Route>
-    </Routes>
-  );
+                {/* Mista entre ususarios */}
+                <Route element={<RequireAuth
+                    allowedRoles={[ROLES.Aluno, ROLES.Administrador, ROLES.Coordenador, ROLES.Professor]}/>}>
+                    <Route path="/" element={<Home/>}/>
+                </Route>
+                <Route element={<RequireAuth
+                    allowedRoles={[ROLES.Aluno, ROLES.Administrador, ROLES.Coordenador, ROLES.Professor]}/>}>
+                    <Route path="/editar-avatar" element={<AvatarEdit/>}/>
+                </Route>
+
+                <Route
+                    element={<RequireAuth allowedRoles={[ROLES.Administrador, ROLES.Coordenador, ROLES.Professor]}/>}>
+                    <Route path="editor" element={<Editor/>}/>
+                </Route>
+
+                <Route element={<RequireAuth allowedRoles={[ROLES.Administrador, ROLES.Coordenador]}/>}>
+                    <Route path="admin" element={<Admin/>}/>
+                </Route>
+
+                <Route
+                    element={<RequireAuth allowedRoles={[ROLES.Administrador, ROLES.Coordenador, ROLES.Professor]}/>}>
+                    <Route path="lounge" element={<Lounge/>}/>
+                </Route>
+
+                <Route element={<RequireAuth allowedRoles={[ROLES.Administrador, ROLES.Coordenador]}/>}>
+                    <Route path="/usuarios" element={<Usuarios/>}/>
+                </Route>
+
+                <Route element={<RequireAuth allowedRoles={[ROLES.Administrador, ROLES.Coordenador]}/>}>
+                    <Route path="/alunos" element={<Alunos/>}/>
+                </Route>
+
+                <Route element={<RequireAuth allowedRoles={[ROLES.Administrador, ROLES.Coordenador]}/>}>
+                    <Route path="/mapas" element={<Mapa/>}/>
+                </Route>
+
+                <Route element={<RequireAuth allowedRoles={[ROLES.Administrador, ROLES.Coordenador]}/>}>
+                    <Route path="/questionarios" element={<Questionarios/>}/>
+                </Route>
+
+                {/* Somente para Administradores */}
+                <Route element={<RequireAuth allowedRoles={[ROLES.Administrador]}/>}>
+                    <Route path="/dashboard" element={<Dashboard/>}/>
+                </Route>
+
+                {/* Somente para Coordenadores */}
+
+                {/* Somente para Professores */}
+
+                {/* Somente para Alunos */}
+
+
+                {/* Liberada */}
+                <Route path="*" element={<Missing/>}/>
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
---
+++ src/components/avatarEdit/AvatarEdit.js
@@ -0,0 +1,312 @@
+import React, {useContext, useEffect, useState} from "react";
+import {AvatarOptions} from "./AvatarOptions";
+import AuthContext from "../../context/AuthProvider";
+import Avatar from "avataaars";
+import "./AvatarEdit.module.css";
+import style from "../home/Home.module.css";
+import {Col, Row} from "react-bootstrap";
+import axios from "axios";
+import useAuth from "../../hooks/useAuth";
+
+const AvatarEdit = () => {
+    const API_URL = "https://enadejava-1685497331322.azurewebsites.net";
+    const AVATAR_URL = "/api/v1/atualizar-avatar";
+    const [avatar, setAvatar] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+    const {auth} = useContext(AuthContext);
+    const { setAuth } = useAuth();
+    const {usuario} = auth;
+    const {accessToken} = auth;
+    let user;
+
+    useEffect(() => {
+
+        user = usuario;
+        const usuarioAvatar = usuario.avatar;
+
+        setAvatar(usuarioAvatar);
+        setIsLoading(false);
+    }, []);
+    const generateRandomAvatar = () => {
+        const newAvatar = {};
+        for (const option in AvatarOptions) {
+            const options = AvatarOptions[option];
+            const randomIndex = Math.floor(Math.random() * options.length);
+            const selectedOption = options[randomIndex].value;
+            newAvatar[option] = selectedOption;
+        }
+
+        setAvatar(newAvatar);
+    };
+
+    const updateAvatarProperty = (property, value) => {
+        setAvatar((prevAvatar) => ({
+            ...prevAvatar,
+            [property]: value,
+        }));
+    };
+
+    const saveAvatar = () => {
+        user.avatar = avatar;
+
+         axios.post( API_URL + AVATAR_URL, user,
+             {
+                 headers: {
+                     "Content-Type": "application/json",
+                     "Authorization": `Bearer ${accessToken}`,
+                 },
+             })
+            .then(response => {
+                console.log(response);
+                setAuth({ user: usuario.login, password: usuario.password , roles: usuario.roles[0], accessToken, usuario: user });
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+
+        console.log("Saved Avatar:", avatar);
+    };
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <>
+        <div className={style.fullScreen}>
+            <Row style={{display: "flex", justifyContent: "center"}}>
+                <h2>Personalize seu Avatar</h2>
+            </Row>
+
+            <div>
+                <Row style={{display: "flex", justifyContent: "center"}}>
+                    <Col md={6}>
+                        <Avatar
+                            className="avataaar"
+                            avatarStyle="Circle"
+                            topType={avatar.topType}
+                            accessoriesType={avatar.accessoriesType}
+                            hairColor={avatar.hairColor}
+                            facialHairType={avatar.facialHairType}
+                            clotheType={avatar.clotheType}
+                            clotheColor={avatar.clotheColor}
+                            eyeType={avatar.eyeType}
+                            eyebrowType={avatar.eyebrowType}
+                            mouthType={avatar.mouthType}
+                            skinColor={avatar.skinColor}
+                        />
+                    </Col>
+                    <Col md={6} style={{display: "flex", justifyContent: "center"}}>
+                        <Row style={{display: "flex", justifyContent: "center"}}>
+                            <Col md={6}>
+                                {/* Rest of the options */}
+                                <div>
+                                    <h3> Tipo de camiseta </h3>
+                                    <select
+                                        value={avatar.clotheType}
+                                        onChange={(event) =>
+                                            updateAvatarProperty("clotheType", event.target.value)
+                                        }
+                                    >
+                                        {AvatarOptions.clotheType.map((option) => (
+                                            <option key={option.value} value={option.value}>
+                                                {option.label}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                                {/* Rest of the options */}
+                                <div>
+                                    <h3> Cor da camiseta </h3>
+                                    <select
+                                        value={avatar.clotheColor}
+                                        onChange={(event) =>
+                                            updateAvatarProperty("clotheColor", event.target.value)
+                                        }
+                                    >
+                                        {AvatarOptions.clotheColor.map((option) => (
+                                            <option key={option.value} value={option.value}>
+                                                {option.label}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                                {/* Rest of the options */}
+                                <div>
+                                    <h3> Estampa camiseta </h3>
+                                    <select
+                                        value={avatar.graphicType}
+                                        onChange={(event) =>
+                                            updateAvatarProperty("graphicType", event.target.value)
+                                        }
+                                    >
+                                        {AvatarOptions.graphicType.map((option) => (
+                                            <option key={option.value} value={option.value}>
+                                                {option.label}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                                {/* Rest of the options */}
+                                <div>
+                                    <h3> Olhar </h3>
+                                    <select
+                                        value={avatar.eyeType}
+                                        onChange={(event) =>
+                                            updateAvatarProperty("eyeType", event.target.value)
+                                        }
+                                    >
+                                        {AvatarOptions.eyeType.map((option) => (
+                                            <option key={option.value} value={option.value}>
+                                                {option.label}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                                {/* Rest of the options */}
+                                <div>
+                                    <h3> Sombrancelha </h3>
+                                    <select
+                                        value={avatar.eyebrowType}
+                                        onChange={(event) =>
+                                            updateAvatarProperty("eyebrowType", event.target.value)
+                                        }
+                                    >
+                                        {AvatarOptions.eyebrowType.map((option) => (
+                                            <option key={option.value} value={option.value}>
+                                                {option.label}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                                {/* Rest of the options */}
+                                <div>
+                                    <h3> Boca </h3>
+                                    <select
+                                        value={avatar.mouthType}
+                                        onChange={(event) =>
+                                            updateAvatarProperty("mouthType", event.target.value)
+                                        }
+                                    >
+                                        {AvatarOptions.mouthType.map((option) => (
+                                            <option key={option.value} value={option.value}>
+                                                {option.label}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Col md={6}>
+                            <div>
+                                <h3> Tom da pele </h3>
+                                <select
+                                    value={avatar.topType}
+                                    onChange={(event) =>
+                                        updateAvatarProperty("skinColor", event.target.value)
+                                    }
+                                >
+                                    {AvatarOptions.skinColor.map((option) => (
+                                        <option key={option.value} value={option.value}>
+                                            {option.label}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                            {/* Rest of the options */}
+                            <div>
+                                <h3> Cabelo e acessórios </h3>
+                                <select
+                                    value={avatar.topType}
+                                    onChange={(event) =>
+                                        updateAvatarProperty("topType", event.target.value)
+                                    }
+                                >
+                                    {AvatarOptions.topType.map((option) => (
+                                        <option key={option.value} value={option.value}>
+                                            {option.label}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                            {/* Rest of the options */}
+                            <div>
+                                <h3> Óculos </h3>
+                                <select
+                                    value={avatar.accessoriesType}
+                                    onChange={(event) =>
+                                        updateAvatarProperty("accessoriesType", event.target.value)
+                                    }
+                                >
+                                    {AvatarOptions.accessoriesType.map((option) => (
+                                        <option key={option.value} value={option.value}>
+                                            {option.label}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                            {/* Rest of the options */}
+                            <div>
+                                <h3> Cor do chapéu </h3>
+                                <select
+                                    value={avatar.hatColor}
+                                    onChange={(event) =>
+                                        updateAvatarProperty("hatColor", event.target.value)
+                                    }
+                                >
+                                    {AvatarOptions.hatColor.map((option) => (
+                                        <option key={option.value} value={option.value}>
+                                            {option.label}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                            {/* Rest of the options */}
+                            <div>
+                                <h3> Tipo de barba </h3>
+                                <select
+                                    value={avatar.facialHairType}
+                                    onChange={(event) =>
+                                        updateAvatarProperty("facialHairType", event.target.value)
+                                    }
+                                >
+                                    {AvatarOptions.facialHairType.map((option) => (
+                                        <option key={option.value} value={option.value}>
+                                            {option.label}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                            {/* Rest of the options */}
+                            <div>
+                                <h3> Cor da barba </h3>
+                                <select
+                                    value={avatar.facialHairColor}
+                                    onChange={(event) =>
+                                        updateAvatarProperty("facialHairColor", event.target.value)
+                                    }
+                                >
+                                    {AvatarOptions.facialHairColor.map((option) => (
+                                        <option key={option.value} value={option.value}>
+                                            {option.label}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                        </Col>
+                    </Col>
+                </Row>
+
+
+            </div>
+            <Row style={{display: "flex", justifyContent: "center"}}>
+                <button onClick={generateRandomAvatar}>Gerar aleatório</button>
+                <button onClick={saveAvatar}>Salvar</button>
+            </Row>
+
+        </div>
+        </>
+    );
+};
+
+export default AvatarEdit;
