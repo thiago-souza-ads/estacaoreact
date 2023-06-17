aa7664f87afc69559249c66e3a90a8080a1f4cca
rohdengg <rohdengg@gmail.com>
2023-06-17 15:13:29 -0300

    Home aluno - under development

--- src/components/home/homeImpl/homeAluno/HomeAluno.js
+++ src/components/home/homeImpl/homeAluno/HomeAluno.js
@@ -24,26 +24,31 @@ export const HomeAluno = () => {
                     </Nav.Link>
                 </Nav>
             </Navbar>
+            
             <Container fluid>
+                <Container fluid>
+                    <Row>
+                        <Col md={12} className={styles.welcomeMenu}>
+                            <Col md={2}>
+                             Bem-Vindo! Juliana Marciel
+                            </Col>
+                            <Col md={2}>
+                                <button>Configurar seu avatar</button>
+                            </Col>
+                            <Col md={2}>
+                                <button>Iniciar jornada</button>
+                            </Col>
+                        </Col>  
+                    </Row>
+                </Container>
+
                 <Col md={12} className={styles.column12}>
-                    <Card style={{width: "40%", backgroundColor: "whitesmoke", height: "32rem", borderRadius: '10px'}}>
-                        <Card.Body>
-                            <Card.Title style={{color: "black",bold: true}}>Home Aluno</Card.Title>
-                            <Card.Text style={{color: "black",bold: false, textAlign: 'justify', textJustify:'inter-word'}}>
-                                O Exame Nacional de Desempenho dos Estudantes (Enade) é uma ferramenta de avaliação do Governo Federal aplicada por meio do Instituto Nacional de Estudos e Pesquisas Educacionais Anísio Teixeira (Inep). Ele avalia o rendimento dos concluintes dos cursos de graduação em relação aos conteúdos programáticos previstos nas diretrizes curriculares dos cursos, o desenvolvimento de competências e habilidades necessárias ao aprofundamento da formação geral e profissional, e o nível de atualização dos estudantes com relação à realidade brasileira e mundial. O Enade é obrigatório para estudantes ingressantes e concluintes habilitados de cursos de bacharelado e superiores de tecnologia vinculados às áreas de avaliação da edição. A situação de regularidade do estudante é registrada no histórico escolar. O Enade tem enorme importância na avaliação das Instituições de Ensino Superior (IES) pelo Sistema Nacional de Avaliação da Educação Superior (Sinaes), principalmente pelo peso relativo que tem no cômputo dos conceitos avaliativos dos cursos e da própria IES
-                            </Card.Text>
-                            <Card.Link href="/usuarios">Visualizar Usuarios</Card.Link>
-                        </Card.Body>
-                    </Card>
-                    <Card style={{width: "40%", backgroundColor: "whitesmoke", height: "32rem", borderRadius: '10px'}}>
-                        <Card.Body>
-                            <Card.Title style={{color: "black",bold: true}}>Como é feita a prova do Enade?</Card.Title>
-                            <Card.Text style={{color: "black",bold: false, textAlign: 'justify', textJustify:'inter-word'}}>
-                                A prova do Enade é composta por 10 questões de Formação Geral, sendo 8 questões de múltipla escolha e 2 questões discursivas que avaliam aspectos como clareza, coerência, coesão, estratégias argumentativas, utilização de vocabulário adequado e correção gramatical do texto. Além disso, há 30 questões da parte de formação específica da área, sendo 27 questões de múltipla escolha e 3 discursivas. A prova tem duração de 4 horas e é obrigatória para estudantes ingressantes e concluintes habilitados de cursos de bacharelado e superiores de tecnologia vinculados às áreas de avaliação da edição
-                            </Card.Text>
-                            <Card.Link href="/usuarios">Visualizar Usuarios</Card.Link>
-                        </Card.Body>
-                    </Card>
+                    <Col md={5} style={{border: "1px solid black"}}>
+                        <div className={styles.text}>testetestestewtessfdafsafsfafd</div>
+                    </Col>
+                    <Col md={5} style={{border: "1px solid black"}}>
+                        <div>testetestestewtesd</div>
+                    </Col>
                 </Col>
                 <footer className={`footer mt-auto py-3 bg-light ${styles.footer}`}>
                     <Container fluid>
