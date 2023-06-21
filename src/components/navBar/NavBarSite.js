a1359e68c503f1791d71d680be2d762d01782b56
EstacaoEnadeDouglas <estacaoenade@gmail.com>
2023-06-20 22:41:04 -0300

    NavBar refatoração

--- src/components/navBar/NavBarSite.js
+++ src/components/navBar/NavBarSite.js
@@ -3,51 +3,62 @@ import styles from "./NavBarSite.module.css";
 import {Link} from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 import Login from "../login/Login";
+import {
+    faChalkboardTeacher,
+    faHome, faLink,
+    faList,
+    faMap, faNotEqual,
+    faQuestion,
+    faSchool, faSign, faSubscript,
+    faUser
+} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const NavBarSite = () => {
     const {auth} = useAuth();
     const {usuario} = auth;
 
     const rotasCoordenador = [
-        {to: "/", label: "Home"},
-        {to: "/usuarios", label: "Usuários"},
-        {to: "/mapas", label: "Mapas"},
-        {to: "/questionarios", label: "Questionários"},
-        {to: "/professores", label: "Professores"},
-        {to: "/alunos", label: "Alunos"},
-        {to: "/enade", label: "Enade"},
+        {to: "/", label: "Home", icone: faHome},
+        {to: "/usuarios", label: "Usuários", icone: faUser },
+        {to: "/mapas", label: "Mapas", icone: faMap},
+        {to: "/questionarios", label: "Questionários", icone: faQuestion},
+        {to: "/professores", label: "Professores", icone: faChalkboardTeacher},
+        {to: "/alunos", label: "Alunos", icone: faSchool },
+        {to: "/enade", label: "Enade", icone: faList},
     ];
     const rotasProfessor = [
-        {to: "/", label: "Home"},
-        {to: "/usuarios", label: "Usuários"},
-        {to: "/mapas", label: "Mapas"},
-        {to: "/questionarios", label: "Questionários"},
-        {to: "/professores", label: "Professores"},
-        {to: "/alunos", label: "Alunos"},
-        {to: "/enade", label: "Enade"},
+        {to: "/", label: "Home", icone: faHome},
+        {to: "/usuarios", label: "Usuários", icone: faUser},
+        {to: "/mapas", label: "Mapas", icone: faMap},
+        {to: "/questionarios", label: "Questionários", icone: faQuestion},
+        {to: "/professores", label: "Professores", icone: faChalkboardTeacher},
+        {to: "/alunos", label: "Alunos", icone: faSchool},
+        {to: "/enade", label: "Enade", icone: faList},
     ];
 
     const rotasAlunos = [
-        {to: "/", label: "Home"},
-        {to: "/usuarios", label: "Usuários"},
-        {to: "/mapas", label: "Mapas"},
-        {to: "/questionarios", label: "Questionários"},
-        {to: "/professores", label: "Professores"},
+        {to: "/", label: "Home", icone: faHome},
+        {to: "/usuarios", label: "Usuários", icone: faUser},
+        {to: "/mapas", label: "Mapas", icone: faMap},
+        {to: "/questionarios", label: "Questionários", icone: faQuestion},
+        {to: "/professores", label: "Professores", icone: faChalkboardTeacher},
     ];
     const rotasAdministrador = [
-        {to: "/usuarios", label: "Usuários"},
-        {to: "/mapas", label: "Mapas"},
-        {to: "/questionarios", label: "Questionários"},
-        {to: "/professores", label: "Professores"},
-        {to: "/alunos", label: "Alunos"},
-        {to: "/enade", label: "Enade"},
+        {to: "/usuarios", label: "Usuários", icone: faUser},
+        {to: "/mapas", label: "Mapas", icone: faMap},
+        {to: "/questionarios", label: "Questionários", icone: faQuestion},
+        {to: "/professores", label: "Professores", icone: faChalkboardTeacher},
+        {to: "/alunos", label: "Alunos", icone: faSchool},
+        {to: "/enade", label: "Enade", icone: faList},
     ];
     const rotasPublicas = [
-        {to: "/login", label: "Login"},
-        {to: "/register", label: "Cadastre-se"},
-        {to: "/missing", label: "Tá perdido"},
-        {to: "/linkpage", label: "Links"},
-        {to: "/unauthorized", label: "Não autorizado"}
+        {to: "/", label: "Home", icone: faHome},
+        {to: "/login", label: "Login", icone: faSign},
+        {to: "/register", label: "Cadastre-se", icone: faSubscript},
+        {to: "/missing", label: "Tá perdido", icone: faNotEqual},
+        {to: "/linkpage", label: "Links", icone: faLink},
+        {to: "/unauthorized", label: "Não autorizado", faNotEqual}
     ];
 
     let rotas = [];
@@ -74,6 +85,7 @@ const NavBarSite = () => {
             <Nav className={`mr-auto ${styles.navbarNav}`}>
                 {rotas.map((rota, index) => (
                     <Nav.Link as={Link} to={rota.to} key={index}>
+                        <FontAwesomeIcon icon={rota.icone} className={styles.navIcon} />
                         {rota.label}
                     </Nav.Link>
                 ))}
