ecd0f18487105c623cce708a904a64db42a15737
EstacaoEnadeDouglas <estacaoenade@gmail.com>
2023-06-20 22:48:02 -0300

    Ajuste tela de usuarios

--- src/components/usuarios/Usuarios.js
+++ src/components/usuarios/Usuarios.js
@@ -4,6 +4,7 @@ import useAuth from "../../hooks/useAuth";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEdit, faTrash, faUndo} from "@fortawesome/free-solid-svg-icons";
 import "./Usuarios.module.css";
+import style from "../home/Home.module.css";
 
 const Usuarios = () => {
     const [users, setUsers] = useState([]);
@@ -117,79 +118,82 @@ const Usuarios = () => {
     }
 
     return (
-        <div className="container mt-5 justify-content-center text-center">
-            <h1 className="text-center">Lista de Usuários</h1>
-            {loading ? (
-                <div>Carregando...</div>
-            ) : (
-                <table className="table table-striped table-bordered table-width">
-                    <thead className="table-header">
-                    <tr>
-                        <th className="column-id">ID</th>
-                        <th className="column-nome">Nome</th>
-                        <th className="column-login">Login</th>
-                        <th className="column-role">Perfil</th>
-                        <th className="column-situacao">Situação</th>
-                        <th className="column-editar">Editar</th>
-                        <th className="column-excluir">Excluir</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {users.length > 0 ? (
-                        users.map((user) => (
-                            <tr key={user.id}>
-                                <td className="column-id text-right">{user.id}</td>
-                                <td className="column-nome text-left">{user.nome}</td>
-                                <td className="column-login text-left">{user.login}</td>
-                                <td className="column-role">
-                                    <select
-                                        className="form-select"
-                                        value={user.roles[0]?.nome}
-                                        onChange={(e) => roleChange(user, e.target.value)}
-                                    >
-                                        <option value="">Selecionar</option>
-                                        <option value="Administrador">Administrador</option>
-                                        <option value="Coordenador">Coordenador</option>
-                                        <option value="Professor">Professor</option>
-                                        <option value="Aluno">Aluno</option>
-                                    </select>
-                                </td>
-                                <td className="column-situacao text-center">
-                                    {user.ativo ? "Ativo" : <span className="text-danger">Inativo</span>}
-                                </td>
-                                <td className="column-editar">
-                                    <FontAwesomeIcon
-                                        icon={faEdit}
-                                        onClick={() => editarUsuario(user.id)}
-                                    />
-                                </td>
-                                <td className="column-excluir">
-                                    <FontAwesomeIcon
-                                        icon={user.ativo ? faTrash : faUndo}
-                                        onClick={() => excluirUsuario(user.id)}
-                                        className={user.ativo ? "" : "text-danger"}
-                                    />
-                                </td>
-                            </tr>
-                        ))
-                    ) : (
-                        <tr>
-                            <td colSpan="7">Nenhum usuário encontrado.</td>
-                        </tr>
-                    )}
-                    </tbody>
-                    <tfoot className="table-footer">
-                    <tr>
-                        <td colSpan="7">Total de Usuários: {users.length}</td>
-                    </tr>
-                    </tfoot>
-                </table>
-            )}
+       <div className={style.fullScreen}>
+           <div className="container mt-5 justify-content-center text-center justify-content-center" style={{width: "100%", height: "100%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+               <h1 className="text-center" style={{color: "#010303", fontSize: "40px", textAlign:  "center"}}>Lista de Usuários</h1>
+               {loading ? (
+                   <div>Carregando...</div>
+               ) : (
+                   <table className="table table-striped table-bordered table-width"style={{ color: "#010303", fontSize: "20px", textAlign:  "center", width:  "80%", borderRadius:  "10px", backgroundColor: "#E5E5E5"}}>
+                       <thead className="table-header">
+                       <tr>
+                           <th className="column-id" style={{ color: "#010303", fontSize: "20px", textAlign:  "center"}}>ID</th>
+                           <th className="column-nome" style={{ color: "#010303", fontSize: "20px", textAlign:  "center"}}>Nome</th>
+                           <th className="column-login" style={{ color: "#010303", fontSize: "20px", textAlign:  "center"}}>Login</th>
+                           <th className="column-role" style={{ color: "#010303", fontSize: "20px", textAlign:  "center"}}>Perfil</th>
+                           <th className="column-situacao" style={{ color: "#010303", fontSize: "20px", textAlign:  "center"}}>Situação</th>
+                           <th className="column-editar" style={{ color: "#010303", fontSize: "20px", textAlign:  "center"}}>Editar</th>
+                           <th className="column-excluir" style={{ color: "#010303", fontSize: "20px", textAlign:  "center"}}>Excluir</th>
+                       </tr>
+                       </thead>
+                       <tbody>
+                       {users.length > 0 ? (
+                           users.map((user) => (
+                               <tr key={user.id}>
+                                   <td className="column-id text-right">{user.id}</td>
+                                   <td className="column-nome text-left">{user.nome}</td>
+                                   <td className="column-login text-left">{user.login}</td>
+                                   <td className="column-role">
+                                       <select
+                                           className="form-select"
+                                           value={user.roles[0]?.nome}
+                                           onChange={(e) => roleChange(user, e.target.value)}
+                                       >
+                                           <option value="">Selecionar</option>
+                                           <option value="Administrador">Administrador</option>
+                                           <option value="Coordenador">Coordenador</option>
+                                           <option value="Professor">Professor</option>
+                                           <option value="Aluno">Aluno</option>
+                                       </select>
+                                   </td>
+                                   <td className="column-situacao text-center">
+                                       {user.ativo ? "Ativo" : <span className="text-danger">Inativo</span>}
+                                   </td>
+                                   <td className="column-editar">
+                                       <FontAwesomeIcon
+                                           icon={faEdit}
+                                           onClick={() => editarUsuario(user.id)}
+                                       />
+                                   </td>
+                                   <td className="column-excluir" style={{color: "#f50909", fontSize: "20px", textAlign:  "center"}}>
+                                       <FontAwesomeIcon
+                                           icon={user.ativo ? faTrash : faUndo}
+                                           onClick={() => excluirUsuario(user.id)}
+                                           className={user.ativo ? "" : "text-danger"}
+                                       />
+                                   </td>
+                               </tr>
+                           ))
+                       ) : (
+                           <tr>
+                               <td colSpan="7">Nenhum usuário encontrado.</td>
+                           </tr>
+                       )}
+                       </tbody>
+                       <tfoot className="table-footer">
+                       <tr>
+                           <td colSpan="7">Total de Usuários: {users.length}</td>
+                       </tr>
+                       </tfoot>
+                   </table>
+               )}
 
-            <button className="btn btn-primary" onClick={criarUsuario}>
-                Adicionar Usuário
-            </button>
+               <button className="btn btn-primary" onClick={criarUsuario}>
+                   Adicionar Usuário
+               </button>
+           </div>
         </div>
+
     );
 
 
