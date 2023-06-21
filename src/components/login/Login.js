6b71dd472664e8703b0d198760505eaad650c51c
EstacaoEnadeDouglas <estacaoenade@gmail.com>
2023-06-21 19:56:30 -0300

    Chamada da tela de cadastro

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -106,6 +106,7 @@ const Login = () => {
                         </div>
                         <button>Logar</button>
                         <div className={styles.error}>{errMsg}</div>
+                        <p> Não possui conta ainda? <a href="/register">Cadastre-se</a></p>
                     </form>
                 </div>
             </div>
